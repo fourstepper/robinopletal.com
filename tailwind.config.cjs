85dadd89282d962d1fc7a62c0ede71c86d29d19f
Robin Opletal <me@robinopletal.com>
2023-08-22 09:47:22 +0200

    feat(ui): change light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,24 +11,34 @@ const config = {
   daisyui: {
     logs: false,
       darkTheme: "custom-dark",
-      themes: ["garden",
+      themes: [
         // TODO: figure out all the colors in the custom dark mode
         {
-          "custom-dark": {
-            "primary": "#adf989",
-            "secondary": "#ede18b",
-            "accent": "#c69e25",
-            "neutral": "#171c27",
-            "base-100": "#323843",
-            "info": "#96bff8",
-            "success": "#6aecdd",
-            "warning": "#f0b624",
-            "error": "#ea628b",
-          },
-        },
-      ],
-  },
-
+            "custom-light": {
+                "primary": "#c94740",
+                "secondary": "#9dd657",
+                "accent": "#45e0d6",
+                "neutral": "#1c1f26",
+                "base-100": "#f7f7f7",
+                "info": "#a1c6e8",
+                "success": "#22c55e",
+                "warning": "#eaad34",
+                "error": "#ef1e1a",
+            },
+            "custom-dark": {
+                "primary": "#adf989",
+                "secondary": "#ede18b",
+                "accent": "#c69e25",
+                "neutral": "#171c27",
+                "base-100": "#323843",
+                "info": "#96bff8",
+                "success": "#6aecdd",
+                "warning": "#f0b624",
+                "error": "#ea628b",
+              },
+            },
+        ],
+    },
   plugins: [daisyui],
 };
 
