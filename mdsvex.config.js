b875b0f7baad599f7439815cba15021add466986
Robin Opletal <me@robinopletal.com>
2023-08-25 12:46:05 +0200

    feat(mdsvex): auto link headings

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,4 +1,6 @@
 import { defineMDSveXConfig as defineConfig } from "mdsvex";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from "rehype-slug";
 
 const config = defineConfig({
   "extensions": [".md"],
@@ -11,7 +13,7 @@ const config = defineConfig({
   },
 
   "remarkPlugins": [],
-  "rehypePlugins": []
+  "rehypePlugins": [rehypeSlug, rehypeAutolinkHeadings]
 });
 
 export default config;
