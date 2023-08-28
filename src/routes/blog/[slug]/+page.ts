45205d4e9db3d5b3186b37f27c2046b7e48c2128
Robin Opletal <me@robinopletal.com>
2023-08-28 09:39:12 +0200

    feat(blog): initial work and dynamicity

---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,23 @@
+import { error } from '@sveltejs/kit';
+
+export async function load({ params } : { params: any }){
+	let post
+	try {
+		post = await import(`../../../lib/posts/${params.slug}.md`)
+	} catch {
+		throw error(404, "Post not found");
+	}
+
+	try {
+	const { title, date } = post.metadata
+	const content = post.default
+
+	return {
+		content,
+		title,
+		date,
+	}
+	} catch {
+		throw error(500, "Something went wrong while loading the post. Please contact the site administrator.");
+	}
+}
--- src/routes/posts/+page.svelte
+++
@@ -1,5 +0,0 @@
-<section class="container mx-auto mb-auto px-4 text-center text-2xl">
-  <div>
-    <h1>Posts</h1>
-  </div>
-</section>
