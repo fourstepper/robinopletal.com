267595f7a6fab5c33b54b6b63271892080f5343a
Robin Opletal <me@robinopletal.com>
2023-08-30 11:41:45 +0200

    chore(config): remove unused postsPerPage const

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,9 +4,6 @@ export const siteURL = "robinopletal.com";
 export const siteLink = "https://robinopletal.com";
 export const siteAuthor = "Robin Opletal";
 
-// Controls how many posts are shown per page on the main blog index pages
-export const postsPerPage = 10;
-
 export const navItems = [
   {
     title: "Blog",
