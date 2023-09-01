4b32faa8a3013c9410a426997ac23b546d553ff1
Robin Opletal <me@robinopletal.com>
2023-09-01 08:58:12 +0200

    fix(opengraph): siteLink variable

--- src/routes/posts/+layout.svelte
+++ src/routes/posts/+layout.svelte
@@ -1,15 +1,13 @@
+<script lang="ts">
+  import { siteLink } from "$lib/config";
+</script>
+
 <svelte:head>
   <meta property="og:title" content="Blog | Robin Opletal" />
-  <meta
-    property="og:image"
-    content="https://robinopletal.com/opengraph/og-image-blog.png"
-  />
+  <meta property="og:image" content="{siteLink}/opengraph/og-image-blog.png" />
 
   <meta name="twitter:title" content="Blog | Robin Opletal" />
-  <meta
-    name="twitter:image"
-    content="https://robinopletal.com/opengraph/og-image-blog.png"
-  />
+  <meta name="twitter:image" content="{siteLink}/opengraph/og-image-blog.png" />
 </svelte:head>
 
 <slot />
