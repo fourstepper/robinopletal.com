3c6d5adeeadd9f04c036548845a38aebf724e199
Robin Opletal <me@robinopletal.com>
2023-09-02 21:52:05 +0200

    fix(posts.json): remove `limit` option

--- src/routes/api/posts.json/+server.ts
+++ src/routes/api/posts.json/+server.ts
@@ -4,9 +4,7 @@ import { json } from "@sveltejs/kit";
 export const prerender = true;
 
 export const GET = async () => {
-  const options = {
-    limit: 10,
-  };
+  const options = {};
 
   const { posts } = await fetchPosts(options);
   return json(posts);
