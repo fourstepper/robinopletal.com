f87a364e8df2b4cb21e7084ab0e31bb42a82d95e
Robin Opletal <me@robinopletal.com>
2023-11-17 12:08:48 +0000

    feat(site): prepare for deployment to my docker host (#3)
    This PR can't be merged until we are live on Vercel - it wouldn't break anything, however once this merges (especially https://codeberg.org/fourstepper/robinopletal.com/commit/6efedd4c9480a87d109ccc0d1b7c5f9fbff2ae8c), we won't be able to deploy to Vercel anymore.
    
    Reviewed-on: https://codeberg.org/fourstepper/robinopletal.com/pulls/3

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,12 +12,7 @@ const config = {
   preprocess: [vitePreprocess({}), mdsvex(mdsvexConfig)],
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      runtime: "nodejs18.x",
-    }),
+      adapter: adapter(),
   }
 };
 
