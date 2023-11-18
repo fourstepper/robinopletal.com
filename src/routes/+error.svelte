8f3a99a1b918fb9d24759d520c68641a00854689
Robin Opletal <me@robinopletal.com>
2023-11-18 12:43:26 +0100

    feat(error): show status code and title in tab title

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { siteTitle, siteDescription } from "$lib/config";
 </script>
+
+<svelte:head>
+  <title>{$page.status} | {siteTitle}</title>
+  <meta name="description" content={siteDescription} />
+</svelte:head>
 
 <h1 class="mx-auto mb-auto text-4xl">
   {$page.status}: {$page.error?.message}
